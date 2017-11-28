93ad7f0ad172bedb12d063fdbaea74bf5b774abc
Pablo Santofimia <>
2017-11-28 23:07:23 +0100

    Prueba Subida por Pablo Santofimia

---
+++ Hello_World_Pablo/src/Hello_World_Pablo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Hello_World_Pablo.cpp
+// Author      : Pablo Santofimia
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World, soy Pablo Santofimia!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
